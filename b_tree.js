fec66d3b0f777cf84cf9d02a8e221e799be58836
Nathan Herzing <nherzing@gmail.com>
2016-07-23 17:41:42 -0400

    Somewhat working btree

---
+++ b_tree.js
@@ -0,0 +1,97 @@
+class Node {
+  constructor(keys=[], children=[]) {
+    this._keys = keys;
+    this._children = children;
+    this.parent = null;
+  }
+
+  keyCount() {
+    return this._keys.length;
+  }
+
+  children() {
+    return this._children;
+  }
+
+  includes(key) {
+    return this._keys.includes(key) ||
+      this._children.some(node => node.includes(key));
+  }
+
+  insert(key) {
+    const childIdx = this._childIdxFor(key);
+    this._keys.splice(childIdx, 0, key);
+  }
+
+  leafFor(key) {
+    if (this._children.length === 0) {
+      return this;
+    }
+    return this._children[this._childIdxFor(key)].leafFor(key);
+  }
+
+  split() {
+    const medianIdx = Math.floor(this._keys.length / 2),
+          leftNode = new Node(this._keys.slice(0, medianIdx), this._children.slice(0, medianIdx + 1)),
+          rightNode = new Node(this._keys.slice(medianIdx + 1), this._children.slice(medianIdx + 1));
+
+    leftNode._children.forEach(c => c.parent = leftNode);
+    rightNode._children.forEach(c => c.parent = rightNode);
+    return [this._keys[medianIdx], leftNode, rightNode];
+  }
+
+  insertChildBefore(key, node) {
+    this._children[this._keys.indexOf(key)] = node;
+    node.parent = this;
+  }
+
+  insertChildAfter(key, node) {
+    const idx = this._keys.indexOf(key);
+    this._children.splice(idx+1, 0, node);
+    node.parent = this;
+  }
+
+  _childIdxFor(key) {
+    const idx = this._keys.findIndex(nodeKey => nodeKey > key);
+    return idx === -1 ? this._keys.length : idx;
+  }
+}
+
+export default class BTree {
+  constructor(order) {
+    this.order = order;
+    this.root = new Node([]);
+  }
+
+  insert(key) {
+    if (this.includes(key)) {
+      return;
+    }
+    const leaf = this.root.leafFor(key);
+    leaf.insert(key);
+    this._splitUp(leaf);
+  }
+
+  includes(key) {
+    return this.root.includes(key);
+  }
+
+  toString() {
+    return JSON.stringify(this.root, ["_keys", "_children"], " ");
+  }
+
+  _splitUp(node) {
+    while (node !== null && node.keyCount() >= this.order) {
+      const [splitKey, leftNode, rightNode] = node.split();
+      if (node.parent === null) {
+        node.parent = new Node([]);
+        this.root.parent = node.parent;
+        this.root = node.parent;
+      }
+      node.parent.insert(splitKey);
+      node.parent.insertChildBefore(splitKey, leftNode);
+      node.parent.insertChildAfter(splitKey, rightNode);
+      node = node.parent;
+    }
+  }
+}
---
+++ test/b_tree_test.js
@@ -0,0 +1,108 @@
+import { assert } from "chai";
+import BTree from "../b_tree.js";
+
+// order = max # children
+// at most order-1 keys per node
+// every node has at least (order-1)/2
+
+function verifyNode(node, order, isRoot=false) {
+  const numChildren = node.children().length,
+        isLeaf = numChildren === 0;
+  assert(numChildren <= order, `Too many children, were ${numChildren}, limit ${order}`);
+  if (!isLeaf) {
+    assert(numChildren >= Math.ceil(order / 2), "Too few children");
+  }
+  if (isRoot && !isLeaf) {
+    assert(numChildren >= 2);
+  }
+  assert(node.keyCount() < order, "Too many keys");
+  if (!isLeaf) {
+    assert(numChildren === (node.keyCount() + 1), `Has ${node.keyCount()} key(s) but ${numChildren} children.`);
+  }
+  for (let child of node.children()) {
+    verifyNode(child, order);
+  }
+}
+
+function verifyTree(tree) {
+  verifyNode(tree.root, tree.order, true);
+}
+
+function bulkInsert(tree, ...keys) {
+  keys.forEach(key => tree.insert(key));
+}
+
+function assertIncludes(tree, ...keys) {
+  keys.forEach(key => assert(tree.includes(key), `Can"t find value ${key}.`));
+}
+
+describe("new tree", () => {
+  it("makes a valid empty tree", () => {
+    verifyTree(new BTree(3));
+  });
+});
+
+describe("insertion & searching", () => {
+  it("inserts a single element correctly", () => {
+    const tree = new BTree(3);
+    tree.insert(1);
+    assertIncludes(tree, 1);
+    verifyTree(tree);
+  });
+
+  it("inserts 2 elements correctly", () => {
+    const tree = new BTree(3);
+    bulkInsert(tree, 1, 2);
+    assertIncludes(tree, 1, 2);
+    verifyTree(tree);
+  });
+
+  it("inserts 3 elements correctly", () => {
+    const tree = new BTree(3);
+    bulkInsert(tree, 1, 2, 3);
+    assertIncludes(tree, 1, 2, 3);
+    verifyTree(tree);
+  });
+
+  it("inserts 4 elements correctly", () => {
+    const tree = new BTree(3);
+    bulkInsert(tree, 1, 2, 3, 4);
+    assertIncludes(tree, 1, 2, 3, 4);
+    verifyTree(tree);
+  });
+
+  it("inserts 7 elements correctly, requiring cascading splits", () => {
+    const tree = new BTree(3);
+    bulkInsert(tree, 1, 2, 3, 4, 5, 6, 7);
+    assertIncludes(tree, 1, 2, 3, 4, 5, 6, 7);
+    verifyTree(tree);
+  });
+
+  it("inserts 10 random elements", () => {
+    const tree = new BTree(3),
+          keys = [5, 8, 2, 4, 0];
+    bulkInsert(tree, ...keys);
+    assertIncludes(tree, ...keys);
+    verifyTree(tree);
+  });
+
+  it("inserts 20 random elements", () => {
+    const tree = new BTree(3),
+          keys = [278,347,161,833,693,358,95,874,816,621,271,326,73,295];
+    bulkInsert(tree, ...keys);
+    assertIncludes(tree, ...keys);
+    verifyTree(tree);
+  });
+
+  function randomSeq(len) {
+    return Array(len).fill().map(i => Math.floor(Math.random() * 100000));
+  }
+
+  it("inserts a whole bunch of elements", () => {
+    const tree = new BTree(3),
+          keys = randomSeq(1000);
+    bulkInsert(tree, ...keys);
+    assertIncludes(tree, ...keys);
+    verifyTree(tree);
+  });
+});
